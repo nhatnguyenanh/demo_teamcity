23420297ea8a13d79adff1922fc0f4afb43a978e
nhatnguyenopswat <nhat.nguyen@opswat.com>
2023-08-31 16:09:11 +0700

    test env branch

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 
 if __name__ == '__main__':
+
     check_env = os.getenv("CI_CD")
     branch = os.getenv("branch")
     if check_env:
-        print(f"hello trigger {check_env} with branch {branch} ")
+        print(f"hello trigger 1 {check_env} with branch {branch} ")
     else:
         print("hello teamcity master")
