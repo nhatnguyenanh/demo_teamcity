9496a55583501a36bb1aca288c4759c00320aaeb
nhatnguyenopswat <nhat.nguyen@opswat.com>
2023-08-31 14:42:54 +0700

    test env

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import os
+
 if __name__ == '__main__':
-    print("hello teamcity master")
+    check_env = os.getenv("CI/CD")
+    if check_env:
+        print(f"hello {check_env} teamcity master")
+    else:
+        print("hello teamcity master")
