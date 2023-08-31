3faf21fb59ecc7d2950b5dc06d027159707a76ca
nhatnguyenopswat <nhat.nguyen@opswat.com>
2023-08-31 17:02:50 +0700

    test env branch

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     start_time = time.monotonic()
     branch = os.getenv("branch")
     print(f"=========== checkout from {branch} ===========")
+    print(f"=========== test trigger 12345 ===========")
     sleep_time = os.getenv("SLEEP_TIME")
     if not sleep_time:
         sys.exit(0)
